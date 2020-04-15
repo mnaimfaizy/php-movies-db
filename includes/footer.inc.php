e582abb8a1ff61802a05d3813739ba963be6564f
Mohammad Naim Faizy <mnaim.faizy@gmail.com>
2020-04-15 05:50:44 +0430

    chore(public): modified footer to look good and active

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -9,38 +9,45 @@
 		<div class="gp-container">
             <div class="footer-widget footer-1">
             	<div class="wpb_wrapper">
-					<img src="images/f_logo.png" alt=""/>
+					<img src="images/logo.png" alt=""/>
 				</div> 
 	          <br>
-	          <p>It is a long established fact that a reader will be distracted by the readable content of a page.</p>
-			  <p class="text">There are many variations of passages of Lorem Ipsum available, but the majority have suffered.</p>
+	          <p>A simple yet useful database to catalog your favorite movies and know what was the faviroute one.</p>
+			  <p class="text">It can also be used as a website for your shop or online store with just adding some modifications.</p>
+			  <p>&nbsp;</p>
 			 </div>
 			 <div class="footer_box">
 			  <div class="col_1_of_3 span_1_of_3">
 					<h3>Categories</h3>
 					<ul class="first">
-						<li><a href="#">Dance</a></li>
-						<li><a href="#">History</a></li>
-						<li><a href="#">Specials</a></li>
+						<li><a href="http://localhost/php-movies-db/movie.php?genre_id=2">Adventure</a></li>
+						<li><a href="http://localhost/php-movies-db/movie.php?genre_id=4">Biography</a></li>
+						<li><a href="http://localhost/php-movies-db/movie.php?genre_id=15">Musical</a></li>
+						<li><a href="http://localhost/php-movies-db/movie.php?genre_id=6">Comedy</a></li>
 					</ul>
 		     </div>
 		     <div class="col_1_of_3 span_1_of_3">
 					<h3>Information</h3>
 					<ul class="first">
-						<li><a href="#">New products</a></li>
-						<li><a href="#">top sellers</a></li>
-						<li><a href="#">Specials</a></li>
+						<li><a href="movie.php">New Movies</a></li>
+						<li><a href="trailers.php">Trailers</a></li>
+						<li><a href="search.php">Search Movie</a></li>
+						<li><a href="contact.php">Contact Us</a></li>
 					</ul>
 		     </div>
 		     <div class="col_1_of_3 span_1_of_3">
 					<h3>Follow Us</h3>
 					<ul class="first">
-						<li><a href="#">Facebook</a></li>
-						<li><a href="#">Twitter</a></li>
-						<li><a href="#">Youtube</a></li>
+						<li><a href="https://www.facebook.com/mnaimfaizy" target="_blank">Facebook</a></li>
+						<li><a href="https://twitter.com/Naim_Soft" target="_blank">Twitter</a></li>
+						<li><a href="https://www.linkedin.com/in/mohammad-naim-faizy-17600250" target="_blank">LinkedIn</a></li>
+						<li><a href="https://github.com/mnaimfaizy" target="_blank">GitHub</a></li>
 					</ul>
 					<div class="copy">
-				      <p>&copy; 2015 Template by <a href="http://w3layouts.com" target="_blank"> w3layouts</a></p>
+				      <p>&copy; 2015 Template by <a href="http://w3layouts.com" target="_blank"> w3layouts</a>.</p>
+			        </div>
+					<div class="copy">
+				      <p> Developed by <a href="https://mnfprofile.com" target="_blank">Mohammad Naim Faizy</a>.</p>
 			        </div>
 		     </div>
 		    <div class="clearfix"> </div>
